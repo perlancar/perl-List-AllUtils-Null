723a04d64dc0ae5eeb4ad4b661982cc19ea5bddd
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-04-01 21:08:36 +0700

    Initial commit

---
+++ lib/List/AllUtils/Null.pm
@@ -0,0 +1,98 @@
+package List::AllUtils::Null;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = qw(max maxstr min minstr sum);
+
+sub sum(@) {
+    return undef unless @_;
+    my $s = 0;
+    for (@_) {
+        return undef unless defined;
+        $s += $_;
+    }
+    $s;
+}
+
+sub min (@) {
+    return undef unless @_;
+    my $min = shift;
+    return undef unless defined $min;
+    for (@_) {
+        return undef unless defined;
+        $_ < $min and $min = $_;
+    }
+    $min;
+}
+
+sub max (@) {
+    return undef unless @_;
+    my $max = shift;
+    return undef unless defined $max;
+    for (@_) {
+        return undef unless defined;
+        $_ > $max and $max = $_;
+    }
+    $max;
+}
+
+sub minstr (@) {
+    return undef unless @_;
+    my $min = shift;
+    return undef unless defined $min;
+    for (@_) {
+        return undef unless defined;
+        $_ lt $min and $min = $_;
+    }
+    $min;
+}
+
+sub maxstr (@) {
+    return undef unless @_;
+    my $max = shift;
+    return undef unless defined $max;
+    for (@_) {
+        return undef unless defined;
+        $_ gt $max and $max = $_;
+    }
+    $max;
+}
+
+1;
+# ABSTRACT: List subroutines that treat undef as contagious unknown, like null in SQL
+
+=head1 SYNOPSIS
+
+ use List::AllUtils::Null qw(
+     max maxstr min minstr
+     sum
+ );
+
+ say max(1,2,3,4,5);     # => 5
+ say max(1,2,undef,4,5); # => undef
+
+ say min(1,2,3,4,5);     # => 1
+ say min(1,2,undef,4,5); # => undef
+
+ say sum(1,2,3,4,5);     # => 15
+ say sum(1,2,undef,4,5); # => undef
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+Your favorite SQL reference.
+
+L<List::Util> and friends (L<List::SomeUtils>, L<List::UtilsBy>,
+L<List::MoreUtils>, L<List::AllUtils>).
+
+=cut
---
+++ lib/List/Util/Null.pm
@@ -0,0 +1,22 @@
+package List::Util::Null;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use alias::module 'List::AllUtils::Null';
+
+1;
+# ABSTRACT: Alias for List::AllUtils::Null
+
+=head1 SYNOPSIS
+
+Use L<List::AllUtils::Null> instead.
+
+
+=head1 SEE ALSO
+
+L<List::AllUtils::Null>
+
+=cut
